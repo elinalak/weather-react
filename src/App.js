a3cbb02847c59dc00482677b42ee4ece9087009f
elinalak <lakida_v@icloud.com>
2022-09-28 08:21:29 +0200

    Fixed App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Form from "./Form";
+import Main from "./Main";
 import "./App.css";
 import React from "react";
 
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Form />
+        <Main />
       </header>
     </div>
   );
