a82f1045b50aa434945e61902e8e0078621739bf
elinalak <lakida_v@icloud.com>
2022-09-30 10:23:25 +0200

    Optimized propName at APP.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 function App() {
   return (
     <div className="App">
-      <Main city="Krakow" />
+      <Main cityDefault="Krakow" />
       <Footer />
     </div>
   );
--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
-// import { HashLoader } from "react-spinners";
+import { HashLoader } from "react-spinners";
 import WeatherForecast from "./WeatherForecast";
 import "bootstrap/dist/css/bootstrap.css";
 import WeatherInfo from "./WeatherInfo";
 
 export default function Main(props) {
-  let [city, setCity] = useState(props.city);
-  let [weather, setWeather] = useState({});
+  let [city, setCity] = useState(props.cityDefault);
+  let [weather, setWeather] = useState({ ready: false });
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -23,11 +23,12 @@ export default function Main(props) {
       .trim()
       .toLowerCase()}&appid=${keyAPI}&units=metric`;
     axios.get(url).then(showWeather);
-    console.log(url);
+    console.log(`Function is here`, url);
   }
 
   function showWeather(response) {
     setWeather({
+      ready: true,
       temperature: Math.round(response.data.main.temp),
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
@@ -39,7 +40,31 @@ export default function Main(props) {
     console.log(`City is ${weather.city}`);
   }
 
-  // if (weather.ready)
+  if (weather.ready)
+    return (
+      <div>
+        <div className="main-container">
+          <form className="row" onSubmit={handleSubmit}>
+            <input
+              type="search"
+              className="col-sm-8 input-form text-capitalize"
+              placeholder="Type a city"
+              autoFocus="on"
+              onChange={handleChangeCity}
+            />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary col-sm-3"
+            />
+          </form>
+          <div className="weather-local"></div>
+          <WeatherInfo weather={weather} />
+          <WeatherForecast />
+        </div>
+      </div>
+    );
+  else handleChange();
   return (
     <div>
       <div className="main-container">
@@ -57,42 +82,16 @@ export default function Main(props) {
             className="btn btn-primary col-sm-3"
           />
         </form>
-        <div className="weather-local"></div>
-        <WeatherInfo weather={weather} />
-        <WeatherForecast />
+        <h1 className="mt-3">Weather checker</h1>
+        <h6>Loading... ⛱</h6>
+        <HashLoader
+          className="d-block m-auto mt-3"
+          size={20}
+          thickness={100}
+          speed={100}
+          color="lightblue"
+        />
       </div>
     </div>
   );
-  // else
-  //   return (
-  //     <div>
-  //       <div className="main-container">
-  //         <form className="row" onSubmit={handleSubmit}>
-  //           <input
-  //             type="search"
-  //             className="col-sm-8 input-form text-capitalize"
-  //             placeholder="Type a city"
-  //             autoFocus="on"
-  //             onChange={handleChangeCity}
-  //           />
-  //           <input
-  //             type="submit"
-  //             value="Search"
-  //             className="btn btn-primary col-sm-3"
-  //           />
-  //         </form>
-  //         <div className="weather-local">
-  //           <h1 className="mt-3">Weather checker</h1>
-  //           <h6>Try to input a city name correctly ⛱</h6>
-  //           <HashLoader
-  //             className="d-block m-auto mt-3"
-  //             size={20}
-  //             thickness={100}
-  //             speed={100}
-  //             color="lightblue"
-  //           />
-  //         </div>
-  //       </div>
-  //   </div>
-  // );
 }
