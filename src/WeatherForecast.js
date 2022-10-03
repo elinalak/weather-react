1fa5d9ac93728282854bd12033c6b8fe7d7a7f24
elinalak <lakida_v@icloud.com>
2022-10-03 17:18:06 +0200

    Fixed WeatherForecast bootsrtap

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -6,7 +6,7 @@ export default function WeatherForecast() {
   function DayForecast(props) {
     return (
       <div className="mt-3 ">
-        <div className="col-xxl-2 current-data weather-forecast container-fluid">
+        <div className="col current-data weather-forecast container-fluid">
           <p>{props.time}</p>
           <WeatherIcons code="50d" />
           <p className="mt-3">
@@ -19,7 +19,7 @@ export default function WeatherForecast() {
   }
 
   return (
-    <div className="d-inline-flex day-container">
+    <div className="d-inline-flex day-container justify-content-center">
       <DayForecast time="15:00" daytemp={4} nighttemp={2} />
       <DayForecast time="18:00" daytemp={5} nighttemp={4} />
       <DayForecast time="21:00" daytemp={5} nighttemp={4} />
